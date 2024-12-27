e6db09efa721cd62aef141d5d8b5e898264a860f
Izunnaya <ugorjimicheal.um@gmail.com>
2024-12-27 11:22:09 +0100

    Created all necessary components & page routes

---
+++ app/cart/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CartItem from "../components/CartItem";
+
+const CartPage = () => {
+  return (
+    <div>
+      Cart page details
+      <CartItem />
+    </div>
+  );
+};
+
+export default CartPage;
---
+++ app/components/index.js
@@ -0,0 +1,7 @@
+export { default as Herobanner } from "./Herobanner";
+export { default as Layout } from "./Layout";
+export { default as Navbar } from "./Navbar";
+export { default as Categories } from "./Categories";
+export { default as CartItems } from "./CartItem";
+export { default as ProductCard } from "./ProductCard";
+export { default as footer } from "./Footer.jsx";
